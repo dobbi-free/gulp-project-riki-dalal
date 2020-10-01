d13bea123744d43643a6a862a8cc667e27affb46
dobbi_free <gangb6444@gmail.com>
2020-10-01 14:53:53 +0300

    v4.0

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -1,13 +1,12 @@
 function deviceNav() {
-    $('.sendwich').click(function() {
+    $('.sendwich').click(function () {
         $(this).find('.sendwich-btn').toggleClass('active');
         $(this).siblings('ul').toggleClass('active');
     });
 };
 
 
-
-$(document).ready(function() {
+$(document).ready(function () {
     deviceNav();
 
     $('.about__slider').slick({
@@ -38,16 +37,114 @@ $(document).ready(function() {
         slidesToShow: 5,
         slidesToScroll: 3,
         autoplay: true,
+
     });
 
-    $('.slider-sight').slick({
-        dots: false,
-        infinite: true,
-        arrows: true,
-        speed: 300,
+    $('.product__slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
-        nextArrow: '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  width = "40px"  height = "40px" >    <path fill-rule="evenodd"  fill="rgb(102, 102, 102)" d="M20.322,40.001 C9.470,40.001 0.641,31.177 0.641,20.331 C0.641,9.484 9.470,0.660 20.322,0.660 C31.173,0.660 40.001,9.484 40.001,20.331 C40.001,31.177 31.173,40.001 20.322,40.001 ZM20.322,1.795 C10.096,1.795 1.778,10.110 1.778,20.331 C1.778,30.550 10.096,38.864 20.322,38.864 C30.546,38.864 38.865,30.550 38.865,20.331 C38.865,10.110 30.546,1.795 20.322,1.795 ZM20.322,28.338 C20.176,28.338 20.031,28.282 19.920,28.171 C19.698,27.949 19.698,27.590 19.920,27.368 L26.393,20.899 L13.637,20.899 C13.323,20.899 13.069,20.645 13.069,20.331 C13.069,20.017 13.323,19.762 13.637,19.762 L26.393,19.762 L19.920,13.294 C19.698,13.071 19.698,12.712 19.920,12.490 C20.142,12.268 20.502,12.268 20.724,12.490 L28.569,20.331 L20.724,28.171 C20.613,28.282 20.467,28.338 20.322,28.338 Z"/> </svg>',
-        prevArrow: '<svg  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  width = "40px"  height = "40px" >    <path fill-rule="evenodd"  fill="rgb(102, 102, 102)" d="M20.322,40.001 C9.470,40.001 0.641,31.177 0.641,20.331 C0.641,9.484 9.470,0.660 20.322,0.660 C31.173,0.660 40.001,9.484 40.001,20.331 C40.001,31.177 31.173,40.001 20.322,40.001 ZM20.322,1.795 C10.096,1.795 1.778,10.110 1.778,20.331 C1.778,30.550 10.096,38.864 20.322,38.864 C30.546,38.864 38.865,30.550 38.865,20.331 C38.865,10.110 30.546,1.795 20.322,1.795 ZM20.322,28.338 C20.176,28.338 20.031,28.282 19.920,28.171 C19.698,27.949 19.698,27.590 19.920,27.368 L26.393,20.899 L13.637,20.899 C13.323,20.899 13.069,20.645 13.069,20.331 C13.069,20.017 13.323,19.762 13.637,19.762 L26.393,19.762 L19.920,13.294 C19.698,13.071 19.698,12.712 19.920,12.490 C20.142,12.268 20.502,12.268 20.724,12.490 L28.569,20.331 L20.724,28.171 C20.613,28.282 20.467,28.338 20.322,28.338 Z"/> </svg>',    });
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+
+    $('.slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.product__slider',
+        dots: false,
+        arrows: true,
+        centerMode: false,
+        focusOnSelect: true,
+        vertical: true,
+        verticalSwiping: true,
+        nextArrow: '.next-arrow',
+        prevArrow: '.prev-arrow',
+    });
+
+    $('.product__slider-bottom').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        nextArrow: '.next-arrow__bottom',
+        prevArrow: '.prev-arrow__bottom',
+        asNavFor: '.slider-nav__bottom'
+    });
+
+    $('.slider-nav__bottom').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        asNavFor: '.product__slider',
+        dots: false,
+        arrows: false,
+        centerMode: false,
+        focusOnSelect: true,
+        vertical: false,
+        verticalSwiping: false,
+
+    });
 });
+
+function backToTop() {
+    let button = $('.back-to-top');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 50) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+
+    $(button).click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({scrollTop: 0}, "slow")
+    })
+}
+
+backToTop()
+
+
+let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header');
+    let selectItem = document.querySelectorAll('.select__item');
+
+
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle);
+    });
+
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
+    }
+
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.select'),
+            currentText = select.querySelector('.select__current');
+        currentText.innerText = text;
+        select.classList.remove('is-active');
+
+    }
+
+};
+
+$(document).mouseup(function (e) {
+    var container = $(".select");
+    container.removeClass('is-active');
+    if (container.has(e.target).length === 0) {
+        container.removeClass('is-active');
+    }
+});
+
+
+select();
+
+
+
+
